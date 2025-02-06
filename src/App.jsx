c74897b2a9f31d769943166cb3e295293a4da22c
DuongTrungQuoc <quoccccc36@gmai.com>
2025-02-06 14:27:49 +0700

    build UI for Header

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import FeatureMovies from "./components/FeatureMovies";
+// API Read Access Token
+// eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZWMxMjI5N2FiYjVjNTgxOGNlODFhN2Y3MmU2ZmVkYyIsIm5iZiI6MTczODc4ODQ0MS4yNCwic3ViIjoiNjdhM2NlNTlkNDdiYzc3Y2I4ODU5YTk5Iiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.zzRUON7EflyguqvKdtoCGio9RlogBqhm38_yxvJZdUU
+// API Key: 3ec12297abb5c5818ce81a7f72e6fedc
+function App() {
+  return (
+    <>
+      <Header />
+      <FeatureMovies />
+    </>
+  );
+}
+
+export default App;
